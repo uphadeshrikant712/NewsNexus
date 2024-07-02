ef92f990687f8dc66e252a09f3de00b08dbaf00a
Uphade Shrikant Bhagwat <uphadeshrikant712@gmail.com>
2024-07-03 02:51:46 +0530

    again changed

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import NewsItem from './NewsItem';
 
-const NewsBoard = ({category}) => {
-
+const NewsBoard = ({ category }) => {
   const [articles, setArticles] = useState([]);
+
   useEffect(() => {
     const fetchNews = async () => {
-      const url = `https://newsapi.org/v2/top-headlines?country=IN&category=${category}&apiKey=5a3f3e17b53949af86b5af8e0d7b4bd0`;
+      const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+      const url = `https://newsapi.org/v2/top-headlines?country=IN&category=${category}&apiKey=${apiKey}`;
 
       try {
         const response = await fetch(url);
@@ -23,15 +24,17 @@ const NewsBoard = ({category}) => {
     fetchNews();
   }, [category]);
 
-
   return (
-    <div className='text-white bg-gradient-to-r
-    from-black via-slate-700 to-black pt-6 '>
+    <div className='text-white bg-gradient-to-r from-black via-slate-700 to-black pt-6'>
       <div className="heading flex items-center justify-center py-2">
-      <h1 className='text-2xl flex text-black font-semibold px-2 bg-gradient-to-r from-pink-600 to-blue-400 font-sriracha'> NewsNexus✨  </h1>
+        <h1 className='text-2xl flex text-black font-semibold px-2 bg-gradient-to-r from-pink-600 to-blue-400 font-sriracha'>
+          NewsNexus✨
+        </h1>
       </div>
-      
-      <h2 className='flex items-center justify-center text-lg font-grotesk text-center px-2 pb-[2vw]'>Your daily source for unbiased, up-to-the-minute news !              </h2>
+
+      <h2 className='flex items-center justify-center text-lg font-grotesk text-center px-2 pb-[2vw]'>
+        Your daily source for unbiased, up-to-the-minute news!
+      </h2>
 
       {articles.map((news) => (
         <NewsItem
@@ -42,9 +45,8 @@ const NewsBoard = ({category}) => {
           url={news.url}
         />
       ))}
-
     </div>
-  )
-}
+  );
+};
 
-export default NewsBoard
+export default NewsBoard;
